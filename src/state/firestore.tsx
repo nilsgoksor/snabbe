3c4cbb0dc80466e37e4dca03c219630bfbd9ea4f
nilsgoksor <nils.goksor@gmail.com>
2020-05-07 16:21:37 +0200

    Firebase config

--- src/state/firestore.tsx
+++ src/state/firestore.tsx
@@ -1,4 +1,5 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDqDuPQdaRtgn7rMlnpFYjsxVdVb9ivl9g",
