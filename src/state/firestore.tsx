12acc81a2a214192a666bfb20de70d9e4606d441
nilsgoksor <nils.goksor@gmail.com>
2020-05-07 09:14:55 +0200

    Added firebase database support

---
+++ src/state/firestore.tsx
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqDuPQdaRtgn7rMlnpFYjsxVdVb9ivl9g",
+  authDomain: "snabbe-9f0f7.firebaseapp.com",
+  databaseURL: "https://snabbe-9f0f7.firebaseio.com",
+  projectId: "snabbe-9f0f7",
+  storageBucket: "snabbe-9f0f7.appspot.com",
+  messagingSenderId: "227212044657",
+  appId: "1:227212044657:web:740c60a320f666ce65eed0",
+  measurementId: "G-33GPRQLLTN",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export default db;
