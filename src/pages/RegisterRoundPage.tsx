aa815e8ca443374b987d377dc9d2f27f1d1de5a2
nilsgoksor <nils.goksor@gmail.com>
2020-07-08 18:59:22 +0200

    RegisterRoundPage simplified. Removed AddPlayer

--- src/pages/RegisterRoundPage.tsx
+++ src/pages/RegisterRoundPage.tsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from "react";
-import AddPlayer from "../components/AddPlayer";
+import React, { useState, useEffect } from "react";
 import RoundDataSummary from "../components/RoundDataSummary";
-import { Button } from "../styled-components/styled-components";
-import { useContextState } from "../state/context";
-import { SET_PLAYERS, SET_ROUND_DATA } from "../state/actionTypes";
+import {
+  Button,
+  Heading1,
+  Heading3,
+} from "../styled-components/styled-components";
 import db from "../state/firestore";
-import { PLAYERS, ROUNDS } from "../constants/routes";
+import styled from "styled-components";
+import AddIcon from "../components/AddIcon";
+import FootballPlayerIcon from "../components/FootballPlayerIcon";
+import { PLAYERS, ROUNDS } from "../constants/endpoints";
 
 const RegisterRoundPage = () => {
-  const { state, dispatch } = useContextState();
-  const { players, roundData } = state;
+  const [players, setPlayers] = useState<string[] | null>(null);
+  const [playersInRound, setPlayersInRound] = useState<string[]>([]);
+  const [roundData, setRoundData] = useState<
+    { name: string; points: number }[]
+  >([]);
+  const [newName, setNewName] = useState<string | null>(null);
+  const [addNewPlayer, setAddNewPlayer] = useState<Boolean>(false);
 
   useEffect(() => {
     db.collection(PLAYERS)
@@ -17,14 +26,26 @@ const RegisterRoundPage = () => {
       .then((querySnapshot) => {
         const data = querySnapshot.docs.map((doc) => doc.data());
         const fetchedPlayers = data.map((player) => {
-          return { name: player.name, initialPoints: player.initialPoints };
+          return player.name;
         });
-        dispatch({
-          type: SET_PLAYERS,
-          players: fetchedPlayers,
-        });
+        setPlayers(fetchedPlayers);
       });
-  }, [roundData, dispatch]);
+  }, [addNewPlayer]);
+
+  useEffect(() => {
+    if (playersInRound) {
+      const round = playersInRound.map((player, index) => {
+        return {
+          name: player,
+          points:
+            index !== 0
+              ? playersInRound.length - index
+              : playersInRound.length + 1,
+        };
+      });
+      setRoundData(round);
+    }
+  }, [playersInRound]);
 
   const currentTime = new Date(Date.now());
 
@@ -36,63 +57,169 @@ const RegisterRoundPage = () => {
   });
 
   return (
-    <>
-      <h1>Register round</h1>
-      <AddPlayer
-        players={players && players.map((p) => p.name)}
-        addToRound={(player: { name: string; points: number }) => {
-          if (player) {
-            const insertIndex = roundData.findIndex(
-              (p) => p.points < player.points
-            );
-            if (insertIndex !== -1) {
-              const modifiedroundData = [...roundData];
-              modifiedroundData.splice(insertIndex, 0, player);
-              dispatch({
-                type: SET_ROUND_DATA,
-                roundData: modifiedroundData,
-              });
-            } else {
-              dispatch({
-                type: SET_ROUND_DATA,
-                roundData: [...roundData, player],
-              });
-            }
-            const newPlayer = !players.find((p) => {
-              return p.name.toUpperCase() === player.name.toUpperCase();
-            });
-
-            if (newPlayer) {
-              db.collection(PLAYERS).doc(player.name).set({
-                name: player.name,
-                initialPoints: 0,
-              });
-            }
-          }
-        }}
-      />
-      {roundData.length > 0 && (
-        <>
-          <h3>{`snabbe on ${formattedCurrentTime}`}</h3>
-          <RoundDataSummary />
-          <Button
+    <RegisterRoundPageContainer>
+      <Heading1>Register round</Heading1>
+      {players && (
+        <PlayersContainer>
+          {players
+            .filter((p) => !playersInRound.includes(p))
+            .map((player, index) => (
+              <Player
+                key={player + index}
+                onClick={() => setPlayersInRound([player, ...playersInRound])}
+              >
+                <FootballPlayerIcon />
+                {player}
+              </Player>
+            ))}
+          <AddIconContainer
             onClick={() => {
-              db.collection(ROUNDS).doc(currentTime.toString()).set({
-                round: roundData,
-                date: Date.now(),
-              });
-              dispatch({
-                type: SET_ROUND_DATA,
-                roundData: [],
-              });
+              if (!addNewPlayer) {
+                document.getElementById("new-player")?.focus();
+              }
+              setAddNewPlayer(!addNewPlayer);
             }}
           >
-            REGISTER
+            <AddIcon />
+          </AddIconContainer>
+        </PlayersContainer>
+      )}
+      {addNewPlayer && (
+        <AddPlayerContainer>
+          <InputPlayerName
+            id="new-player"
+            type="text"
+            autoFocus
+            value={newName || ""}
+            placeholder={"player"}
+            onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+              setNewName(e.target.value.toUpperCase());
+            }}
+          />
+          <Button
+            disabled={!newName}
+            onClick={() => {
+              newName &&
+                db.collection(PLAYERS).doc(newName).set({
+                  name: newName,
+                  initialPoints: 0,
+                });
+              setAddNewPlayer(false);
+              setNewName(null);
+            }}
+          >
+            ADD PLAYER
           </Button>
+        </AddPlayerContainer>
+      )}
+
+      {playersInRound.length > 0 && (
+        <>
+          <Heading3>{`Snabbe ${formattedCurrentTime}`}</Heading3>
+          <RoundDataSummary
+            roundData={roundData}
+            removePlayer={(player) => {
+              const modifiedPlayers = playersInRound.filter(
+                (p) => p !== player?.name
+              );
+              setPlayersInRound(modifiedPlayers);
+            }}
+          />
+          <SubmitRoundContainer>
+            <Button
+              onClick={() => {
+                db.collection(ROUNDS).doc(currentTime.toString()).set({
+                  round: roundData,
+                  date: currentTime.toString(),
+                });
+                setPlayersInRound([]);
+                setRoundData([]);
+              }}
+            >
+              REGISTER ROUND
+            </Button>
+          </SubmitRoundContainer>
         </>
       )}
-    </>
+    </RegisterRoundPageContainer>
   );
 };
 
 export default RegisterRoundPage;
+
+const RegisterRoundPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const PlayersContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const AddPlayerContainer = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const InputPlayerName = styled.input`
+  border: 1px solid black;
+  padding: 15px;
+  outline: none;
+`;
+
+const Player = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 70px;
+  margin: 5px;
+  padding: 5px;
+  background-color: ${(p) => p.theme.colors.white};
+  color: ${(p) => p.theme.colors.green};
+  word-break: break-all;
+
+  path {
+    fill: ${(p) => p.theme.colors.green};
+  }
+
+  :hover {
+    cursor: pointer;
+    opacity: ${(p) => p.theme.hover.opacity};
+  }
+`;
+
+const AddIconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 70px;
+  height: 70px;
+  margin: 5px;
+  padding: 5px;
+  background-color: ${(p) => p.theme.colors.white};
+  color: ${(p) => p.theme.colors.green};
+  path {
+    fill: ${(p) => p.theme.colors.green};
+  }
+
+  :hover {
+    cursor: pointer;
+    background-color: ${(p) => p.theme.colors.green};
+    color: ${(p) => p.theme.colors.white};
+    path {
+      fill: ${(p) => p.theme.colors.white};
+    }
+  }
+`;
+
+const SubmitRoundContainer = styled.div`
+  margin-top: 20px;
+`;
