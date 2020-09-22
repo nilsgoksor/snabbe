887114249aef551320ee89838aad3c24f0df18b0
nilsgoksor <nils.goksor@gmail.com>
2020-09-22 09:45:03 +0200

    Header clean up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Heading = ({ pages, history }) => {
   const [showMenu, setShowMenu] = useState(false);
-  const [showHeader, setShowHeader] = useState(true);
-  const [headerIsTransparent, setHeaderIsTransparent] = useState(true);
-  const [headerIsMovedUp, setHeaderIsMovedUp] = useState(false);
-  const [scrollPos, setScrollPos] = useState(0);
-  const scrollBreakTransparent = 300;
-  const scrollBreakMoveUp = 100;
-
-  const handleScroll = (event) => {
-    const currentScrollPos = scrollPos;
-    setShowHeader(document.body.getBoundingClientRect().top > currentScrollPos);
-    setScrollPos(document.body.getBoundingClientRect().top);
-    setHeaderIsTransparent(
-      window.scrollY < scrollBreakTransparent ? true : false
-    );
-    setHeaderIsMovedUp(window.scrollY > scrollBreakMoveUp ? true : false);
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  });
 
   return (
     <>
-      <ScrollBuffer />
-      <Nav
-        transparent={headerIsTransparent}
-        moveUp={headerIsMovedUp}
-        open={showMenu}
-        show={showHeader}
-      >
-        <IconWrapper>
-          <NavIcon onClick={() => setShowMenu(!showMenu)}>
-            <Line open={showMenu} />
-            <Line open={showMenu} />
-          </NavIcon>
-        </IconWrapper>
+      <Nav open={showMenu}>
+        <NavIcon onClick={() => setShowMenu(!showMenu)}>
+          <Line open={showMenu} />
+          <Line open={showMenu} />
+        </NavIcon>
       </Nav>
       <Overlay open={showMenu}>
         <OverlayMenu open={showMenu}>
@@ -69,35 +40,19 @@ const Heading = ({ pages, history }) => {
 };
 export default Heading;
 
-const ScrollBuffer = styled.div`
-  position: fixed;
-  z-index: -10;
-  width: 100%;
-  height: 50%;
-  background-color: ${(p) => p.theme.colors.green};
-`;
-
 const Nav = styled.nav`
-  width: 100%;
-  position: fixed;
-  z-index: 200;
-  background-color: ${(p) =>
-    p.transparent ? "transparent" : p.theme.colors.green};
   background-color: ${(p) => p.open && "transparent"};
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: center;
-  font-size: 20px;
+  right: 0px;
+  z-index: 200;
+  position: absolute;
   transition: 0.2s ease-in-out;
   transform: translateY(
     ${(p) => (!p.show && p.moveUp && !p.open ? "-100%" : "0%")}
   );
 `;
 
-export const NavIcon = styled.div`
-  margin-right: 15px;
-  margin-top: 7.5px;
+const NavIcon = styled.div`
+  margin-right: 10px;
   display: block;
   background: none;
   cursor: pointer;
@@ -136,7 +91,8 @@ const Overlay = styled.div`
   top: 0;
   z-index: 150;
   transform: translateX(${(p) => (p.open ? "0%" : "+100%")});
-  background-color: white;
+  background-color: ${(p) => p.theme.colors.blue};
+  color: ${(p) => p.theme.colors.red};
   transition: 0.4s ease-in-out;
   visibility: ${(p) => (p.open ? "visible" : "hidden")};
   @media (max-width: ${(p) => p.theme.breakpoints.small}) {
@@ -149,13 +105,12 @@ const OverlayMenu = styled.ul`
   position: absolute;
   text-align: right;
   padding-top: 100px;
-  padding-right: 20px;
 `;
 
 const OverlayText = styled.h3`
   font-size: 60px;
   margin: 40px;
-  color: ${(p) => p.theme.colors.green};
+  color: ${(p) => p.theme.colors.purple};
   text-decoration: ${(p) => p.selected && "underline"};
 
   :hover {
@@ -178,9 +133,4 @@ const Backdrop = styled.div`
   background-color: rgba(0, 0, 0, ${(p) => (p.open ? 0.5 : 0)});
   display: ${(p) => !p.open && "none"};
   transition: 0.2s ease-in-out;
-`;
-
-const IconWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
 `;
