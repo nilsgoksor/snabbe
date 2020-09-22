922f532f6e0d208bab53a9b8971aa078100411d7
nilsgoksor <nils.goksor@gmail.com>
2020-09-22 09:45:57 +0200

    Updated endpoints for new season

--- src/constants/endpoints.tsx
+++ src/constants/endpoints.tsx
@@ -1,4 +1,8 @@
 const devMode = false;
-export const ROUNDS = !devMode ? "rounds" : "mock_rounds";
-export const PLAYERS = !devMode ? "players" : "mock_players";
-export const HISTORY = !devMode ? "history" : "mock_histort";
+const activeSeason = "Fall2020";
+
+export const ROUNDS = !devMode
+  ? `${activeSeason}_rounds`
+  : `${activeSeason}_mock_rounds`;
+export const PLAYERS = !devMode ? `players` : `mock_players`;
+export const HISTORY = !devMode ? "history" : "mock_history";
